50887e900625bd8b7e5e46507fadbb3b8165523b
Kristo Palo <kristopalo@hotmail.com>
2017-02-20 00:25:14 +0200

    major changes

--- src/main/java/ee/ttu/webapps/journal/Handler.java
+++ src/main/java/ee/ttu/webapps/journal/Handler.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
@@ -27,11 +28,14 @@ public class Handler extends HttpServlet {
 			if (handlerAction.equals("addJournalEntry")) {
 				addJournalEntry(request);
 				out.println("Successfully created a new journal entry");
-
+			} else if (handlerAction.equals("editJournalEntry")) {
+				editJournalEntry(request);
+				out.println("Successfully edited a journal entry");
+			} else if (handlerAction.equals("createEditJournalEntryForm")) {
+				createEditJournalEntryForm(request, response);
 			} else if (handlerAction.equals("deleteJournalEntry")) {
 				deleteJournalEntry(request);
 				out.println("Successfully deleted a journal entry");
-
 			} else {
 				throw new JournalException("Handler action '" + handlerAction + "' is not supported at the moment");
 			}
@@ -42,6 +46,121 @@ public class Handler extends HttpServlet {
 		} catch (Exception e) {
 			handleException(e, out, request, response);
 		}
+	}
+
+	private void editJournalEntry(HttpServletRequest request) throws JournalException {
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = getConnection();
+
+			int entryId = new Integer(request.getParameter("entryId")).intValue();
+
+			PreparedStatement ps = con.prepareStatement("UPDATE journaldb.entry SET subject_id = ?, memo = ? WHERE id=?");
+			
+			ps.setInt(1, new Integer(request.getParameter("subjectId")).intValue());
+			ps.setString(2, request.getParameter("memo"));
+			ps.setInt(3, entryId);
+			
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+			throw new JournalException("Failed to edit entry: " + e.getMessage(), e);
+		}
+	}
+
+	private void createEditJournalEntryForm(HttpServletRequest request, HttpServletResponse response)
+			throws JournalException {
+		try {
+
+			response.setContentType("text/html;charset=UTF-8");
+
+			int subjectId = new Integer(request.getParameter("subjectId")).intValue();
+			String username = request.getParameter("username");
+			String memoText = request.getParameter("memo");
+
+			String docType = "<!DOCTYPE html>";
+			String htmlStart = "<html lang=\"en\">";
+			String head = "<head><meta charset=\"utf-8\">" + "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">"
+					+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+					+ "<meta name=\"description\" content=\"\"><meta name=\"author\" content=\"\">"
+					+ "<link rel=\"icon\" href=\"../../favicon.ico\">"
+					+ "<title>Online Journal</title><link href=\"dist/css/bootstrap.min.css\" rel=\"stylesheet\">"
+					+ "<link href=\"styles/signin.css\" rel=\"stylesheet\"><link href=\"styles/navbar.css\" rel=\"stylesheet\"></head>";
+			String bodyStart = "<body>";
+			String container = "<div class=\"container\">";
+			String form = getNewEditEntryForm(username, new Integer(request.getParameter("userId")).intValue(),
+					memoText, subjectId, new Integer(request.getParameter("entryId")).intValue());
+			String jumboEnd = "</div></div>";
+			String bodyEnd = "</body>";
+			String htmlEnd = "</html>";
+
+			StringBuilder sb = new StringBuilder();
+			sb.append(docType);
+			sb.append(htmlStart);
+			sb.append(head);
+			sb.append(bodyStart);
+			sb.append(container);
+			sb.append(form);
+			sb.append(jumboEnd);
+			sb.append(bodyEnd);
+			sb.append(htmlEnd);
+
+			PrintWriter out = response.getWriter();
+			out.println(sb.toString());
+
+		} catch (Exception e) {
+			throw new JournalException("Failed to create edit entry form: " + e.getMessage(), e);
+		}
+	}
+
+	private String getNewEditEntryForm(String username, int userId, String memoText, int subjectId, int entryId) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<form class=\"form-signin\" action=\"handler\" method=\"POST\">");
+		sb.append("<div class=\"jumbotron\"><h2>Edit entry:</h2>");
+		sb.append(getAllSubjects(subjectId));
+		sb.append("<input type=\"text\" class=\"form-control\" placeholder=\"Write something...\" value=\"" + memoText
+				+ "\" name=\"memo\" required>");
+		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"username\" value=\"" + username + "\">");
+		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"userId\" value=" + userId + ">");
+		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"entryId\" value=" + entryId + ">");
+		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"handlerAction\" value=\"editJournalEntry\">");
+		sb.append("<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Save changes</button>");
+		sb.append("</form><br/>");
+
+		return sb.toString();
+	}
+
+	private String getAllSubjects(int subjectId) {
+		StringBuilder sb = new StringBuilder();
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/journaldb", "root", "kristo");
+			PreparedStatement ps = con.prepareStatement("select id, name from journaldb.subject");
+			ResultSet rs = ps.executeQuery();
+			sb.append("<select name=\"subjectId\">");
+			while (rs.next()) {
+				sb.append("<option value=\"" + rs.getInt("id") + "\" " + setSelected(rs.getInt("id"), subjectId) + ">"
+						+ rs.getString("name"));
+
+				sb.append("</option>");
+			}
+			sb.append("</select>");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+
+	private String setSelected(int a, int b) {
+		if (a == b) {
+			return "selected=\"selected\"";
+		}
+		return "";
 	}
 
 	private void handleException(Throwable t, PrintWriter out, HttpServletRequest request,
@@ -66,7 +185,7 @@ public class Handler extends HttpServlet {
 
 			int entryId = new Integer(request.getParameter("entryId")).intValue();
 
-			PreparedStatement ps = con.prepareStatement("DELETE FROM journaldb.entry WHERE id=?;");
+			PreparedStatement ps = con.prepareStatement("DELETE FROM journaldb.entry WHERE id=?");
 			ps.setInt(1, entryId);
 
 			ps.executeUpdate();
--- src/main/java/ee/ttu/webapps/journal/Welcome.java
+++ src/main/java/ee/ttu/webapps/journal/Welcome.java
@@ -17,17 +17,29 @@ public class Welcome extends HttpServlet {
 			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");
 		String username = request.getParameter("username");
-		
+
 		PrintWriter out = response.getWriter();
 
 		StringBuilder sb = new StringBuilder();
 		String docType = "<!DOCTYPE html>";
 		String htmlStart = "<html lang=\"en\">";
-		String head = "<head><meta charset=\"utf-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><meta name=\"description\" content=\"\"><meta name=\"author\" content=\"\"><link rel=\"icon\" href=\"../../favicon.ico\"><title>Online Journal</title><link href=\"dist/css/bootstrap.min.css\" rel=\"stylesheet\"><link href=\"styles/signin.css\" rel=\"stylesheet\"><link href=\"styles/navbar.css\" rel=\"stylesheet\"></head>";
+		String head = "<head><meta charset=\"utf-8\">" + "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">"
+				+ "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+				+ "<meta name=\"description\" content=\"\"><meta name=\"author\" content=\"\">"
+				+ "<link rel=\"icon\" href=\"../../favicon.ico\">"
+				+ "<title>Online Journal</title><link href=\"dist/css/bootstrap.min.css\" rel=\"stylesheet\">"
+				+ "<link href=\"styles/signin.css\" rel=\"stylesheet\"><link href=\"styles/navbar.css\" rel=\"stylesheet\"></head>";
 		String bodyStart = "<body>";
-		
-		String js = "<script>function deleteEntry(id) {document.getElementById(\"entryId\").value = id; document.getElementById(\"deleteEntry\").submit();}</script>";
-		
+
+		String js = "<script>" + "function deleteEntry(id) {" + "document.getElementById(\"entryId\").value = id;"
+				+ "document.getElementById(\"handlerAction\").value = \"deleteJournalEntry\"; "
+				+ "document.getElementById(\"manipulateEntry\").submit();}"
+				+ "function editEntry(id, memoText, subjectId) {" + "document.getElementById(\"entryId\").value = id;"
+				+ "document.getElementById(\"memoText\").value = memoText;"
+				+ "document.getElementById(\"subjectId\").value = subjectId;"
+				+ "document.getElementById(\"handlerAction\").value = \"createEditJournalEntryForm\";"
+				+ "}" + "</script>";
+
 		String container = "<div class=\"container\"><div class=\"jumbotron\"><h1>Welcome back "
 				+ username.substring(0, 1).toUpperCase() + username.substring(1)
 				+ "!</h1><p>Here you can browse your previous entries and make new ones.</p>";
@@ -42,32 +54,32 @@ public class Welcome extends HttpServlet {
 		sb.append(container);
 
 		int userId = getUserId(username);
-		
-		// TODO form for new entries
-		sb.append(getNewEntryForm(username, userId));		
+
+		sb.append(getNewEntryForm(username, userId));
 		sb.append(getUserEntries(userId, username));
-		sb.append(jumboEnd);	
+		sb.append(jumboEnd);
 		sb.append(bodyEnd);
 		sb.append(htmlEnd);
 
 		out.println(sb.toString());
 	}
-	
+
 	private String getNewEntryForm(String username, int userId) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("<form class=\"form-signin\" action=\"handler\" method=\"POST\">");
 		sb.append("<h2 class=\"form-signin-heading\">Make a new entry:</h2>");
 		sb.append(getAllSubjects());
-		sb.append("<input type=\"text\" id=\"inputEmail\" class=\"form-control\" placeholder=\"Write something...\" name=\"memo\" required>");
+		sb.append(
+				"<input type=\"text\" id=\"inputEmail\" class=\"form-control\" placeholder=\"Write something...\" name=\"memo\" required>");
 		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"username\" value=\"" + username + "\">");
 		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"userId\" value=" + userId + ">");
 		sb.append("<input type=\"hidden\" class=\"form-control\" name=\"handlerAction\" value=\"addJournalEntry\">");
 		sb.append("<button class=\"btn btn-lg btn-primary btn-block\" type=\"submit\">Save</button>");
 		sb.append("</form><br/>");
-		
+
 		return sb.toString();
 	}
-	
+
 	private String getAllSubjects() {
 		StringBuilder sb = new StringBuilder();
 		try {
@@ -77,7 +89,7 @@ public class Welcome extends HttpServlet {
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/journaldb", "root", "kristo");
 			PreparedStatement ps = con.prepareStatement("select id, name from journaldb.subject");
 			ResultSet rs = ps.executeQuery();
-			sb.append("<select name=\"subjectId\">");
+			sb.append("<select name=\"subjectId\" class=\"selectpicker\">");
 			while (rs.next()) {
 				sb.append("<option value=\"" + rs.getInt("id") + "\">" + rs.getString("name") + "</option>");
 			}
@@ -102,7 +114,6 @@ public class Welcome extends HttpServlet {
 			if (rs.next()) {
 				res = rs.getInt("id");
 			}
-			
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -118,11 +129,11 @@ public class Welcome extends HttpServlet {
 
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/journaldb", "root", "kristo");
 			PreparedStatement ps = con.prepareStatement(
-					"select r.timestamp, o.name, r.memo, r.id from journaldb.entry r, journaldb.subject o where r.user_id=? and r.subject_id = o.id order by r.timestamp desc");
+					"select r.timestamp, o.name, r.memo, r.id, r.subject_id from journaldb.entry r, journaldb.subject o where r.user_id=? and r.subject_id = o.id order by r.timestamp desc");
 			ps.setInt(1, id);
 			ResultSet rs = ps.executeQuery();
 
-			sb.append("<form action=\"handler\" id=\"deleteEntry\" method=\"POST\">");
+			sb.append("<form action=\"handler\" id=\"manipulateEntry\" method=\"POST\">");
 			sb.append("<table class=\"table table-bordered\">");
 			while (rs.next()) {
 				sb.append("<tr>");
@@ -136,13 +147,20 @@ public class Welcome extends HttpServlet {
 				sb.append(rs.getString("memo"));
 				sb.append("</td>");
 				sb.append("<td>");
-				sb.append("<button class=\"btn btn-danger\" onclick=\"deleteEntry(" + rs.getInt("id") + ")\"><span class=\"glyphicon glyphicon-trash\"></span></button>");
+				sb.append("<button class=\"btn btn-edit\" onclick=\"editEntry(" + rs.getInt("id") + ", "
+						+ "'" + rs.getString("memo") + "'" + ", " + rs.getInt("subject_id")
+						+ ")\"><span class=\"glyphicon glyphicon-edit\"></span></button>");
+				sb.append("<button class=\"btn btn-danger\" onclick=\"deleteEntry("
+						+ rs.getInt("id") + ")\"><span class=\"glyphicon glyphicon-trash\"></span></button>");
 				sb.append("</td>");
 				sb.append("</tr>");
 			}
 			sb.append("</table>");
 			sb.append("<input type=\"hidden\" id=\"entryId\" class=\"form-control\" name=\"entryId\">");
-			sb.append("<input type=\"hidden\" class=\"form-control\" name=\"handlerAction\" value=\"deleteJournalEntry\">");
+			sb.append("<input type=\"hidden\" id=\"memoText\" class=\"form-control\" name=\"memo\">");
+			sb.append("<input type=\"hidden\" id=\"userId\" class=\"form-control\" name=\"userId\" value=\"" + id + "\">");
+			sb.append("<input type=\"hidden\" id=\"subjectId\" class=\"form-control\" name=\"subjectId\">");
+			sb.append("<input type=\"hidden\" id=\"handlerAction\" class=\"form-control\" name=\"handlerAction\">");
 			sb.append("<input type=\"hidden\" class=\"form-control\" name=\"username\" value=\"" + username + "\">");
 			sb.append("</form><br/>");
 
